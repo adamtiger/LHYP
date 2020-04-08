252eeab2a948d217b0ab52839543504038349552
Barnabás Osváth <osvath.barnabas@sonrisa.hu>
2020-04-08 08:52:34 +0200

    data_loader done

--- data_loader.py
+++ data_loader.py
@@ -9,7 +9,7 @@ import os
 class DataLoader:
 	def __init__(self):
 		self.fileLocation = '/userhome/student/lhyp/sample/'
-
+		#self.fileLocation = 'C:\\Users\\Sonrisa\\Desktop\\lhyp\\sample\\'
 
 	def calc_area(self, con):
 		area = 0
@@ -20,30 +20,36 @@ class DataLoader:
 
 	def sort_cons(self, directory):
 		cr = CONreaderVM(directory + '/sa/contours.con')
+		#cr = CONreaderVM(directory + '\\sa\\contours.con')
 		contours = cr.get_hierarchical_contours()
 		dr =  DCMreaderVM(directory + '/sa/images')
+		#dr =  DCMreaderVM(directory + '\\sa\\images')
 		_, __, weight, height, gender = cr.get_volume_data()
 
 		num = []
 		temp_num = []
-
-		for i in range(1,len(contours)):
-			frm_num = list(contours[i])
-			if('ln' in contours[i][frm_num[0]].keys() and 'ln' in contours[i][frm_num[1]].keys()):
-				temp_num.append(i)
+		frm_num = []
+		for i in contours.keys():
+			
+			for j in list(contours[i]):
+				if('ln' in contours[i][j].keys()):
+					temp_num.append(i)
+					frm_num.append(j)
+			
 
 		gap = len(temp_num)//3
-		for i in range(3):
-			num.append(temp_num[0]+i*gap)
-
+		for k in range(2):
+			num.append(temp_num[1]+k*gap)
 
 		with open(directory+"/meta.txt", "r") as meta:
 			pathology = meta.readline().split(' ')[1]
+		# with open(directory+"\\meta.txt", "r") as meta:
+		# 	pathology = meta.readline().split(' ')[1]
 
-		frm_num = list(contours[3])
+		
 
-		if(self.calc_area(contours[3][frm_num[0]]['ln']) 
-			>= self.calc_area(contours[3][frm_num[1]]['ln'])):
+		if(self.calc_area(contours[num[0]][frm_num[0]]['ln']) 
+			>= self.calc_area(contours[num[0]][frm_num[1]]['ln'])):
 			dyastole_fr = frm_num[0]
 			systole_fr = frm_num[1]
 		else:
@@ -64,6 +70,7 @@ class DataLoader:
 
 
 	def picklePatient(self, directory, id):
+		print(id)
 		pathology, weight, height, gender, dy_images, dy_contours, sy_images, sy_contours = self.sort_cons(directory)
 
 		
